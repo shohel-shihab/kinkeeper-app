a44120287a5b71055f3cea76e1017ea4e7a50276
shohel-shihab <shihabcse21@gmail.com>
2026-04-14 21:53:34 +0600

    starting to design kinkeeper-app for that install tailwind and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+
+      <h2 className='text-2xl text-center uppercase'>Welcome to my website</h2>
+
+    </>
+  )
+}
+
+export default App
