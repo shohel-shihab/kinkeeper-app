811c5ec3b5c0451ee0b92b56e9c323b4a083494a
shohel-shihab <shihabcse21@gmail.com>
2026-04-15 13:45:00 +0600

    Design implement of stats according to figma

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,10 +1,55 @@
-import StatsCard from "../components/StatsCard"
+import { useContext } from "react";
+import { AppContext } from "../context/AppProvider";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 
 const Stats = () => {
+    const { timeline } = useContext(AppContext)
+
+    // 🔹 Count interactions
+    const callCount = timeline.filter(t => t.type === "Call").length;
+    const textCount = timeline.filter(t => t.type === "Text").length;
+    const videoCount = timeline.filter(t => t.type === "Video").length;
+
+    // 🔹 Chart data
+    const data = [
+        { name: "Call", value: callCount },
+        { name: "Text", value: textCount },
+        { name: "Video", value: videoCount }
+    ];
+
+    const COLORS = ["#16a34a", "#2563eb", "#9333ea"];
     return (
-        <div>
-            <h2>Hello</h2>
+        <div className="p-6">
+
+            {/* Heading */}
+            <h1 className="text-3xl font-bold mb-6">
+                Friendship Analytics
+            </h1>
+
+            {/* Chart Container */}
+            <div className="bg-white shadow rounded p-6 ">
+                 <h2 className="text-left text-[#244D3F] font-medium text-md">By Interaction Type</h2>
+                <div className="flex justify-center">
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={data}
+                            dataKey="value"
+                            outerRadius={120}
+                            label
+                        >
+                            {data.map((entry, index) => (
+                                <Cell key={index} fill={COLORS[index]} />
+                            ))}
+                        </Pie>
+
+                        <Tooltip />
+                        <Legend />
+                    </PieChart>
+
+                </div>
+            </div>
+
         </div>
     )
 }
